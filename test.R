dc30772ca910f4f664fb0d917a233453ede1c7f0
mdekauwe <mdekauwe@gmail.com>
2019-03-12 18:47:25 +1100

    fixed line length

--- test.R
+++ test.R
@@ -4,11 +4,11 @@ source("R/parameters.R")
 source("R/constants.R")
 
 # Build a params object
-p <- list(Kc25=Kc25, Ko25=Ko25, Eo=Eo, Ec=Ec, Oi=Oi, gamstar25=gamstar25, Eag=Eag, Vcmax25=Vcmax25, 
-          Eav=Eav, deltaSv=deltaSv, Hdv=Hdv, Jmax25=Jmax25, Eaj=Eaj, deltaSj=deltaSj, Hdj=Hdj)
+p <- list(Kc25=Kc25, Ko25=Ko25, Eo=Eo, Ec=Ec, Oi=Oi, gamstar25=gamstar25,
+          Eag=Eag, Vcmax25=Vcmax25, Eav=Eav, deltaSv=deltaSv, Hdv=Hdv,
+          Jmax25=Jmax25, Eaj=Eaj, deltaSj=deltaSj, Hdj=Hdj)
 
 # Met variables ...
 Tleaf <- 25.0 + DEG_2_KELVIN
 
 An <- calc_photosynthesis(p, Tleaf, peaked_Vcmax=TRUE, peaked_Jmax=TRUE)
-

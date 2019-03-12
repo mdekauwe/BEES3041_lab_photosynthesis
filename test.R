c3fb7802f07131ef587388f1c9794eee7fee98c2
mdekauwe <mdekauwe@gmail.com>
2019-03-12 14:02:52 +1100

    Removed c

--- test.R
+++ test.R
@@ -4,7 +4,7 @@ source("R/parameters.R")
 source("R/constants.R")
 
 # Build a params object
-p <- c(list(Kc25=Kc25, Ko25=Ko25, Eo=Eo, Ec=Ec, Oi=Oi))
+p <- list(Kc25=Kc25, Ko25=Ko25, Eo=Eo, Ec=Ec, Oi=Oi)
 
 # Met variables ...
 Tleaf <- 25.0 + DEG_2_KELVIN

be695902bd97891d1d6d808618193890c6b595dc
mdekauwe <mdekauwe@gmail.com>
2019-03-13 23:20:09 +1100

    added first step

--- test.R
+++ test.R
@@ -1,4 +1,5 @@
 
+library(ggplot2)
 source("R/photosynthesis.R")
 source("R/parameters.R")
 source("R/constants.R")
@@ -9,6 +10,20 @@ Tleaf <- 25.0 + DEG_2_KELVIN
 PAR <- 1800.0
 Cs <- 400.
 vpd <- 1.5
+
+Tleaf <- seq(1, 50.0, 0.5) + DEG_2_KELVIN
+Vcmax <- peaked_arrh(p$Vcmax25, p$Eav, Tleaf, p$deltaSv, p$Hdv)
+Jmax <- peaked_arrh(p$Jmax25, p$Eaj, Tleaf, p$deltaSj, p$Hdj)
+
+df <- data.frame(Tleaf, Vcmax, Jmax)
+
+ggplot(df, aes(Tleaf-DEG_2_KELVIN)) +
+  geom_line(aes(y=Vcmax, colour="Vcmax")) +
+  geom_line(aes(y=Jmax, colour="Jmax")) +
+  ylab("Parameter (umol m-2 s-1)") +
+  xlab("Tleaf (deg C)")
+  
+
 
 out <- calc_photosynthesis(p, Tleaf, PAR, Cs, vpd, peaked_Vcmax=TRUE,
                            peaked_Jmax=TRUE)

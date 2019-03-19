2d3e6187d06249d97a459870d448d60e1db28159
mdekauwe <mdekauwe@gmail.com>
2019-03-19 11:08:42 +1100

    Added netcdf stuff

--- R/constants.R
+++ R/constants.R
@@ -9,3 +9,7 @@
 RGAS = 8.314            # Universal gas constant, J mol-1 K-1
 DEG_2_KELVIN = 273.15
 GSC_2_GSW = 1.57        # Ratio of Gsw:Gsc
+SW_2_PAR = 2.3
+UMOL_TO_MOL = 0.000001
+MOL_C_TO_GRAMS_C = 12.
+SEC_2_HLFHR = 1800.
